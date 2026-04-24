51504e11af53b3ab9c537f40b7ce6dc2b00549d8
Hưng Lê <dienhung.leo@gmail.com>
2026-04-24 16:45:14 +0700

    Hardcode Supabase variables for Vercel deployment fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://vpafgqdgkjuztkfajtff.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYWZncWRna2p1enRrZmFqdGZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMDI1MTAsImV4cCI6MjA5MjU3ODUxMH0.RhjqLqqwGjVxiADieIZchmahyiEVrjJVrqSIPiFGkQQ"
+  }
 };
 
 export default nextConfig;
